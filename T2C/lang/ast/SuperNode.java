a3d3dd41480eb69f5e5979fac32e17c492337ae7
gmbdev1993 <97255741+gmbdev1993@users.noreply.github.com>
2022-06-24 22:39:59 -0300

    Add files via upload

---
+++ T2C/lang/ast/SuperNode.java
@@ -0,0 +1,23 @@
+/*
+
+Grupo
+
+Nome: André Dias Nunes
+Matrícula: 201665570C
+
+Nome: Guilherme Barbosa
+Matrícula: 201435031
+
+*/
+
+package lang.ast;
+
+public abstract class SuperNode {
+   
+   // The line and column of the node in the input text
+   
+    public abstract int getLine();
+    public abstract int getColumn();
+}
+
+
